214d0eab6e10e4dc0b689a29659b88f72d5bee60
Shurena <shurena89@gmail.com>
2022-10-09 17:53:57 +0200

    navbar

---
+++ app.js
@@ -0,0 +1,5 @@
+var date = new Date();
+var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
+var current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
+document.getElementById("today").innerHTML = current_date;
+document.getElementById("clock").innerHTML = current_time;
