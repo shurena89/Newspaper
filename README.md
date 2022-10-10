d1bacb1375b5e5df0250a5936edb611157cc8e18
Shurena <shurena89@gmail.com>
2022-10-10 17:29:35 +0200

    final

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Newspaper
 One page newspaper website
 This is one page Newspaper website 
+This website includes modal using Bootstrap 5
--- app.js
+++ app.js
@@ -3,3 +3,5 @@ var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate(
 var current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
 document.getElementById("today").innerHTML = current_date;
 document.getElementById("clock").innerHTML = current_time;
+
+
